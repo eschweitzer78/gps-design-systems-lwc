bd33a6b709b255577e6faba7756b5f9cce8ffaea
Emmanuel Schweitzer <eschweitzer78@yahoo.com>
2022-05-26 19:33:45 +1000

    v1.1

--- sfGpsDs/main/default/lwc/sfGpsDsFormTextareaOsN/sfGpsDsFormTextareaOsN.js
+++ sfGpsDs/main/default/lwc/sfGpsDsFormTextareaOsN/sfGpsDsFormTextareaOsN.js
@@ -5,12 +5,6 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
-/* notes
-      field-level-help-position={_propSetMap.helpTextPos} not supported
-      required-label={allCustomLabelsUtil.OmniRequired} not suppported
-      icon-name="utility:info" not supported
-*/
-
 import OmniscriptTextarea from "omnistudio/omniscriptTextarea";
 import { omniGetMergedField } from "c/sfGpsDsOmniHelpersOsN";
 
