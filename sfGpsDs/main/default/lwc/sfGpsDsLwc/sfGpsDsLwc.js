bc983983be87730574d87a18d3bc80e95cdb14ae
Emmanuel Schweitzer <eschweitzer78@yahoo.com>
2022-05-27 08:12:54 +1000

    v1.1

--- sfGpsDs/main/default/lwc/sfGpsDsLwc/sfGpsDsLwc.js
+++ sfGpsDs/main/default/lwc/sfGpsDsLwc/sfGpsDsLwc.js
@@ -4,6 +4,7 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
+
 import { LightningElement, api, track } from "lwc";
 
 /* IMPORTANT NOTE: if you modify this class, you have to update sfGpsDsLwcOsN
