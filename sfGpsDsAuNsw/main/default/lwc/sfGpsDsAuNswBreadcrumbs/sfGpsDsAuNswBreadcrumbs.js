866a29f55ea4474203060df7a9dff85636e6d674
Emmanuel Schweitzer <eschweitzer78@yahoo.com>
2022-05-27 15:41:50 +1000

    v1.1

--- sfGpsDsAuNsw/main/default/lwc/sfGpsDsAuNswBreadcrumbs/sfGpsDsAuNswBreadcrumbs.js
+++ sfGpsDsAuNsw/main/default/lwc/sfGpsDsAuNswBreadcrumbs/sfGpsDsAuNswBreadcrumbs.js
@@ -4,6 +4,7 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
+
 import { LightningElement, api } from "lwc";
 
 export default class SfGpsDsAuNswBreadcrumbs extends LightningElement {
