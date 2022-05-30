201003740595408204574980638528737d430a89
Emmanuel Schweitzer <eschweitzer78@yahoo.com>
2022-05-31 00:06:57 +1000

    v1.1a

--- sfGpsDsAuNsw/main/default/lwc/sfGpsDsAuNswFormTimeOsN/sfGpsDsAuNswFormTimeOsN.js
+++ sfGpsDsAuNsw/main/default/lwc/sfGpsDsAuNswFormTimeOsN/sfGpsDsAuNswFormTimeOsN.js
@@ -5,12 +5,6 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
-/* notes
-      field-level-help-position={_propSetMap.helpTextPos} not supported
-      required-label={allCustomLabelsUtil.OmniRequired} not suppported
-      icon-name="utility:info" not supported
-*/
-
 import OmniscriptTime from "omnistudio/omniscriptTime";
 import { omniGetMergedField } from "c/sfGpsDsOmniHelpersOsN";
 import tmpl from "./sfGpsDsAuNswFormTimeOsN.html";
