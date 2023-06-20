e9f818b82a24671bc17b17347579e3f3799fce31
Emmanuel Schweitzer <eschweitzer78@yahoo.com>
2023-06-20 13:58:39 +1000

    Commit to build base and NSW DS

--- sfGpsDsAuNsw/main/default/lwc/sfGpsDsAuNswStatusLabel/sfGpsDsAuNswStatusLabel.js
+++ sfGpsDsAuNsw/main/default/lwc/sfGpsDsAuNswStatusLabel/sfGpsDsAuNswStatusLabel.js
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2023, Emmanuel Schweitzer and salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+
 import { LightningElement, api } from "lwc";
 import { computeClass } from "c/sfGpsDsHelpers";
 
