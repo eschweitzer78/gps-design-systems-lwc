a3b184246a5857cf5a31075af7f8bad8faf05ef9
Emmanuel Schweitzer <eschweitzer78@yahoo.com>
2023-08-22 18:30:51 +1000

    Finessing uk error mgt for Date, Time and Datetime

--- sfGpsDsUkGov/main/default/lwc/sfGpsDsUkGovDatePickerOsN/sfGpsDsUkGovDatePickerOsN.js
+++ sfGpsDsUkGov/main/default/lwc/sfGpsDsUkGovDatePickerOsN/sfGpsDsUkGovDatePickerOsN.js
@@ -13,6 +13,8 @@ import { computeClass } from "c/sfGpsDsHelpersOs";
 
 const ORIGINAL_INPUT_SELECTOR = "input[hidden]";
 const DAY_INPUT_SELECTOR = "input[name='date-input-day']";
+const ERROR_ID_SELECTOR = "[data-sf-gps-uk-gov-error-input]";
+const DEBUG = false;
 
 export default class SfGpsDsUkGovDatePickerOsN extends SfGpsDsUkGovLabelMixin(
   OmniscriptDatePicker,
@@ -245,5 +247,34 @@ export default class SfGpsDsUkGovDatePickerOsN extends SfGpsDsUkGovLabelMixin(
       this.hasMonthError ||
       this.hasYearError
     );
+  }
+
+  @api
+  getErrorDetails() {
+    let rv = null;
+
+    let elt = this.template.querySelector(ERROR_ID_SELECTOR);
+
+    if (elt == null) {
+      if (DEBUG)
+        console.log("sfGpsDsUkGovDatePicker: cannot find input element");
+    } else if (this.isCustomLwc) {
+      if (elt.getErrorDetails) {
+        rv = elt.getErrorDetails();
+      } else {
+        if (DEBUG)
+          console.log(
+            "sfGpsDsUkGovDatePicker: child input does not have getErrorDetails"
+          );
+      }
+    }
+    rv = elt
+      ? {
+          id: elt.id,
+          errorMessage: this.errorMessage
+        }
+      : null;
+
+    return rv;
   }
 }
