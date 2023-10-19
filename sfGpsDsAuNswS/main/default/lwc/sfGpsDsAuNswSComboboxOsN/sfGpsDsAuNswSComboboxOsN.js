183f3d08a111dbd8e07f6cb9d46c8e5822bca118
Emmanuel Schweitzer <eschweitzer78@yahoo.com>
2023-10-19 20:41:45 +1000

    Review of NSW S select and combobox for omni 246

--- sfGpsDsAuNswS/main/default/lwc/sfGpsDsAuNswSComboboxOsN/sfGpsDsAuNswSComboboxOsN.js
+++ sfGpsDsAuNswS/main/default/lwc/sfGpsDsAuNswSComboboxOsN/sfGpsDsAuNswSComboboxOsN.js
@@ -5,18 +5,29 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
+import { api } from "lwc";
 import OmnistudioCombobox from "omnistudio/combobox";
 import { computeClass } from "c/sfGpsDsHelpersOs";
 import tmpl from "./sfGpsDsAuNswSComboboxOsN.html";
 
 export default class SfGpsDsAuNswSComboboxOsN extends OmnistudioCombobox {
+  @api labelClassName;
+
   render() {
     return tmpl;
   }
 
+  get computedAriaDescribedBy() {
+    return computeClass({
+      errorMessageBlock: this.isError,
+      helper: this.fieldLevelHelp
+    });
+  }
+
   get computedLabelClassName() {
     return computeClass({
-      "form-required": this.required
+      "form-required": this.required,
+      [this.labelClassName]: this.labelClassName
     });
   }
 }
