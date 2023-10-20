cf47b5b87803192f5145e03f9d213be713b2c0f3
Emmanuel Schweitzer <eschweitzer78@yahoo.com>
2023-10-20 23:27:10 +1000

    Review NSW S Form Textarea omni246

--- sfGpsDsAuNswS/main/default/lwc/sfGpsDsAuNswSTextareaOsN/sfGpsDsAuNswSTextareaOsN.js
+++ sfGpsDsAuNswS/main/default/lwc/sfGpsDsAuNswSTextareaOsN/sfGpsDsAuNswSTextareaOsN.js
@@ -29,8 +29,8 @@ export default class SfGpsDsAuNswSTextareaOsN extends OmniscriptTextarea {
 
   get computedAriaDescribedBy() {
     return computeClass({
-      errorMessageBlock: this.isError,
-      helper: this.fieldLevelHelp
+      helper: this.fieldLevelHelp,
+      errorMessageBlock: this.isError
     });
   }
 }
