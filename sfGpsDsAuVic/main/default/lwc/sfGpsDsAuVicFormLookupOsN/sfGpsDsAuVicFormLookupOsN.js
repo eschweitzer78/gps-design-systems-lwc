58cbde08356b197ee071ef63ec3846e1bf89b0bc
Emmanuel Schweitzer <eschweitzer78@yahoo.com>
2023-10-20 22:01:32 +1000

    Review VIC Form Lookup omni246

--- sfGpsDsAuVic/main/default/lwc/sfGpsDsAuVicFormLookupOsN/sfGpsDsAuVicFormLookupOsN.js
+++ sfGpsDsAuVic/main/default/lwc/sfGpsDsAuVicFormLookupOsN/sfGpsDsAuVicFormLookupOsN.js
@@ -24,4 +24,47 @@ export default class SfGpsDsAuVicFormLookupOsN extends OmniscriptLookup {
   get mergedHelpText() {
     return omniGetMergedField(this, this._handleHelpText);
   }
+
+  get computedListboxClassName() {
+    return computeClass({
+      "sfgpsds-dropdown": true,
+      "sfgpsds-dropdown_fluid": true,
+      "sfgpsds-dropdown_length--5": true,
+      "sfgpsds-predictive__list": true,
+      "sfgpsds-is-open": this.show
+    });
+  }
+
+  /* overriding parent to have smooth scrollInto on focus */
+
+  mouseOverFocus(event) {
+    let index = event.target.getAttribute("data-option-index");
+    this.ariaFocus(index, true);
+  }
+
+  /* overriding parent to cope with theme not being sfgpsds */
+
+  ariaFocus(newIndex, isHover = false) {
+    const options = this.template.querySelectorAll('[role="option"]');
+    if (options.length > 0) {
+      options.forEach((opt) => {
+        opt.classList.remove("sfgpsds-has-focus");
+      });
+    }
+    if (options[newIndex]) {
+      options[newIndex].classList.add("sfgpsds-has-focus");
+      // workaround for an issue where this attribute gets an extra suffix if used in the template
+      this._inputRef.setAttribute(
+        "aria-activedescendant",
+        options[newIndex].id
+      );
+
+      /* scroll into view if keyboard event */
+      if (options[newIndex].scrollIntoView && !isHover) {
+        options[newIndex].scrollIntoView({
+          block: "nearest"
+        });
+      }
+    }
+  }
 }
