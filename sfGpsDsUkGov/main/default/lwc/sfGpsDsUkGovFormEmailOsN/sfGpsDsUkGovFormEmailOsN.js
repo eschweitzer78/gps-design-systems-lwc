98facd8a950f22c727662e3ad75f33462e711c09
Emmanuel Schweitzer <eschweitzer78@yahoo.com>
2023-10-26 18:16:39 +1000

    FC2 Review UK Form Email after error refactor

--- sfGpsDsUkGov/main/default/lwc/sfGpsDsUkGovFormEmailOsN/sfGpsDsUkGovFormEmailOsN.js
+++ sfGpsDsUkGov/main/default/lwc/sfGpsDsUkGovFormEmailOsN/sfGpsDsUkGovFormEmailOsN.js
@@ -5,38 +5,14 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
-import { api } from "lwc";
 import SfGpsDsFormEmailOsN from "c/sfGpsDsFormEmailOsN";
+import SfGpsDsUkGovFormErrorMgtMixin from "c/sfGpsDsUkGovFormErrorMgtMixinOsN";
 import tmpl from "./sfGpsDsUkGovFormEmailOsN.html";
 
-const DEBUG = false;
-
-export default class SfGpsDsUkGovFormEmailOsN extends SfGpsDsFormEmailOsN {
+export default class SfGpsDsUkGovFormEmailOsN extends SfGpsDsUkGovFormErrorMgtMixin(
+  SfGpsDsFormEmailOsN
+) {
   render() {
     return tmpl;
-  }
-
-  @api getErrorDetails() {
-    let elt = this.template.querySelector("[data-omni-input]");
-
-    if (elt) {
-      if (elt.getErrorDetails) {
-        return elt.getErrorDetails();
-      }
-
-      if (DEBUG) console.log("child does not have getErrorDetails api");
-    }
-
-    if (DEBUG) console.log("child not found");
-    return null;
-  }
-
-  @api scrollTo() {
-    const input = this.template.querySelector("[data-omni-input]");
-    input.scrollIntoView({
-      behavior: "smooth",
-      block: "center",
-      inline: "nearest"
-    });
   }
 }
