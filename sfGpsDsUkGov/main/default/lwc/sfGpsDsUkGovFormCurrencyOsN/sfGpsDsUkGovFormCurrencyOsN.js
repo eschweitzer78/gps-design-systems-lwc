54db1a1f591e161c066158f4b552343851efefd1
Emmanuel Schweitzer <eschweitzer78@yahoo.com>
2023-10-26 18:07:02 +1000

    FC2 Review of UK Form Crrency after error refactor

--- sfGpsDsUkGov/main/default/lwc/sfGpsDsUkGovFormCurrencyOsN/sfGpsDsUkGovFormCurrencyOsN.js
+++ sfGpsDsUkGov/main/default/lwc/sfGpsDsUkGovFormCurrencyOsN/sfGpsDsUkGovFormCurrencyOsN.js
@@ -11,38 +11,14 @@
       icon-name="utility:info" not supported
 */
 
-import { api } from "lwc";
 import SfGpsDsFormCurrencyOsN from "c/sfGpsDsFormCurrencyOsN";
+import SfGpsDsUkGovFormErrorMgtMixin from "c/sfGpsDsUkGovFormErrorMgtMixinOsN";
 import tmpl from "./sfGpsDsUkGovFormCurrencyOsN.html";
 
-const DEBUG = false;
-
-export default class SfGpsDsUkGovFormCurrencyOsN extends SfGpsDsFormCurrencyOsN {
+export default class SfGpsDsUkGovFormCurrencyOsN extends SfGpsDsUkGovFormErrorMgtMixin(
+  SfGpsDsFormCurrencyOsN
+) {
   render() {
     return tmpl;
-  }
-
-  @api getErrorDetails() {
-    let elt = this.template.querySelector("[data-omni-input]");
-
-    if (elt) {
-      if (elt.getErrorDetails) {
-        return elt.getErrorDetails();
-      }
-
-      if (DEBUG) console.log("child does not have getErrorDetails api");
-    }
-
-    if (DEBUG) console.log("child not found");
-    return null;
-  }
-
-  @api scrollTo() {
-    const input = this.template.querySelector("[data-omni-input]");
-    input.scrollIntoView({
-      behavior: "smooth",
-      block: "center",
-      inline: "nearest"
-    });
   }
 }
