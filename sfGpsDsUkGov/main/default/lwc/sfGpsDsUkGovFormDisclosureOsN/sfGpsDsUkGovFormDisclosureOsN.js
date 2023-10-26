fbdb6cbcf4b2c69384526ad71adbab90ebee0ac2
Emmanuel Schweitzer <eschweitzer78@yahoo.com>
2023-10-26 19:33:50 +1000

    FC2 Review UK Form Disc error refactor

--- sfGpsDsUkGov/main/default/lwc/sfGpsDsUkGovFormDisclosureOsN/sfGpsDsUkGovFormDisclosureOsN.js
+++ sfGpsDsUkGov/main/default/lwc/sfGpsDsUkGovFormDisclosureOsN/sfGpsDsUkGovFormDisclosureOsN.js
@@ -5,43 +5,24 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
-import { api } from "lwc";
 import SfGpsDsFormDisclosureOsN from "c/sfGpsDsFormDisclosureOsN";
 import SfGpsDsUkGovLabelMixin from "c/sfGpsDsUkGovLabelMixinOsN";
+import SfGpsDsUkGovFormErrorMgtMixin from "c/sfGpsDsUkGovFormErrorMgtMixinOsN";
 import tmpl from "./sfGpsDsUkGovFormDisclosureOsN.html";
 
 const DEFAULT_LABEL_SIZE = "large";
-const DEBUG = false;
 
 export default class SfGpsDsUkGovFormDisclosureOsN extends SfGpsDsUkGovLabelMixin(
-  SfGpsDsFormDisclosureOsN,
+  SfGpsDsUkGovFormErrorMgtMixin(SfGpsDsFormDisclosureOsN),
   DEFAULT_LABEL_SIZE
 ) {
   render() {
     return tmpl;
   }
 
-  @api getErrorDetails() {
-    let elt = this.template.querySelector("[data-omni-input]");
+  initCompVariables() {
+    super.initCompVariables();
 
-    if (elt) {
-      if (elt.getErrorDetails) {
-        return elt.getErrorDetails();
-      }
-
-      if (DEBUG) console.log("child does not have getErrorDetails api");
-    }
-
-    if (DEBUG) console.log("child not found");
-    return null;
-  }
-
-  @api scrollTo() {
-    const input = this.template.querySelector("[data-omni-input]");
-    input.scrollIntoView({
-      behavior: "smooth",
-      block: "center",
-      inline: "nearest"
-    });
+    this.labelSize = this._propSetMap.labelSize;
   }
 }
