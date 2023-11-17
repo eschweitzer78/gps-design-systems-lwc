97bb87f70a0d5335d0c35f1dda7c97dcf94bbf85
Emmanuel Schweitzer <eschweitzer78@yahoo.com>
2023-11-17 10:43:52 +1000

    Date picker fix

--- sfGpsDs/main/default/lwc/sfGpsDsOmniDatePickerOsN/sfGpsDsOmniDatePickerOsN.js
+++ sfGpsDs/main/default/lwc/sfGpsDsOmniDatePickerOsN/sfGpsDsOmniDatePickerOsN.js
@@ -14,6 +14,47 @@ export default class SfGpsDsOmniDatePickerOsN extends SfGpsDsOmniInputMixinOsN(
     super.assignDate(value);
   }
 
+  /* fix required since OS244 when they assume there will be a button with data-id datePickerButton
+    whereas we most probably don't have one */
+
+  _hasDatePickerButton = false;
+
+  _hideCalendar() {
+    if (DEBUG)
+      console.log(
+        CLASS_NAME,
+        "_hideCalendar",
+        `_hasDatePickerButton = ${this._hasDatePickerButton}`
+      );
+
+    if (this._hasDatePickerButton) {
+      super._hideCalendar();
+    } else {
+      if (!this.dropdownTrigger) {
+        return;
+      }
+      if (this.bubbleFocus) {
+        this.dropdownTrigger.removeEventListener(
+          "focusout",
+          this._focusOutHandler
+        );
+        this.dropdownTrigger.removeEventListener(
+          "focusin",
+          this._setInteractionFlag
+        );
+        this.dropdownTrigger.removeEventListener(
+          "mousedown",
+          this._setInteractionFlag
+        );
+
+        delete this.bubbleFocus;
+        //this.template.querySelector("[data-id='datePickerBtn']").focus()
+      }
+      this.dropdownTrigger.classList.remove(`${this.theme}-is-open`);
+      this.render();
+    }
+  }
+
   /***
    * There's an oddity with Omnistudio's datePicker and there are cases when
    * the order attribute assignment ends up happening in leads to the wrong outcome, e.g.
