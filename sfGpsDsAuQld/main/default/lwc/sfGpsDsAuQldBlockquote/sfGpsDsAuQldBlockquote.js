0bdd47ce201287016616380d5bd43c5b8cd3ade9
Emmanuel Schweitzer <eschweitzer78@yahoo.com>
2024-01-11 12:46:57 +1000

    Drop support for QLD and prep for VIC Ripple 2

--- sfGpsDsAuQld/main/default/lwc/sfGpsDsAuQldBlockquote/sfGpsDsAuQldBlockquote.js
+++
@@ -1,8 +0,0 @@
-import { LightningElement, api } from "lwc";
-
-export default class SfGpsDsAuQldBlockquote extends LightningElement {
-  static renderMode = "light";
-
-  @api reference;
-  @api className;
-}
