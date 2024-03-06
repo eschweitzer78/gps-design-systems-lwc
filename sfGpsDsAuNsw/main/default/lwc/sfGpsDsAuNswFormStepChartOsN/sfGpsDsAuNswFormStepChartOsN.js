b204d591c60746867b369c15ab0df2563ea35a60
Emmanuel Schweitzer <eschweitzer78@yahoo.com>
2024-03-06 10:29:29 +1000

    eschweitzer78/fix-305b (#308)
    * Fix 301 and correct issue with Vic1 link
    
    * Change of version for NSW
    
    * Prettier
    
    * Missing deprecated api attribute preventing uppgrades
    
    * Fixing further deprecated api indicator

--- sfGpsDsAuNsw/main/default/lwc/sfGpsDsAuNswFormStepChartOsN/sfGpsDsAuNswFormStepChartOsN.js
+++ sfGpsDsAuNsw/main/default/lwc/sfGpsDsAuNswFormStepChartOsN/sfGpsDsAuNswFormStepChartOsN.js
@@ -9,6 +9,7 @@
 // in omniscript designer, go to setup tab and in the Element Type To LWC Component Mapping section
 // set StepChart as the ElementType and this LWC as the Lightning Web Component
 
+import { api } from "lwc";
 import SfGpsDsFormStepChart from "c/sfGpsDsFormStepChartOsN";
 import tmpl from "./sfGpsDsAuNswFormStepChartOsN.html";
 
@@ -16,4 +17,14 @@ export default class extends SfGpsDsFormStepChart {
   render() {
     return tmpl;
   }
+
+  /* deprecated but have to keep as packaging won't pick up the fact that parent class has it */
+  @api get jsonDef() {
+    return super.jsonDef;
+  }
+
+  set jsonDef(value) {
+    super.jsonDef = value;
+  }
+  /* end deprecated */
 }
