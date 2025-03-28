06c7b78fd593a1b277942ae9e3dd12a58b98fd29
Emmanuel Schweitzer <eschweitzer78@yahoo.com>
2025-03-29 03:47:13 +1000

    QLD build (#424)

--- sfGpsDsAuQld/main/default/lwc/forms/lwc/sfGpsDsAuQldSelectOsN/sfGpsDsAuQldSelectOsN.js
+++ sfGpsDsAuQld/main/default/lwc/forms/lwc/sfGpsDsAuQldSelectOsN/sfGpsDsAuQldSelectOsN.js
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2024-2025, Emmanuel Schweitzer and salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+
 import { LightningElement, api, track } from "lwc";
 import { isArray } from "c/sfGpsDsHelpersOs";
 import OmniscriptPubSub from "omnistudio/pubsub";
