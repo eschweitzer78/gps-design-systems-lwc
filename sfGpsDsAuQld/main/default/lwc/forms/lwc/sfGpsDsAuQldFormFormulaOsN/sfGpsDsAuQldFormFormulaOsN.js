d5d5753735f66ec03d5799a15b3656f479788915
Emmanuel Schweitzer <eschweitzer78@yahoo.com>
2025-03-28 21:18:38 +1000

    QLD proper handling of OS read only

--- sfGpsDsAuQld/main/default/lwc/forms/lwc/sfGpsDsAuQldFormFormulaOsN/sfGpsDsAuQldFormFormulaOsN.js
+++ sfGpsDsAuQld/main/default/lwc/forms/lwc/sfGpsDsAuQldFormFormulaOsN/sfGpsDsAuQldFormFormulaOsN.js
@@ -1,15 +1,9 @@
 /*
- * Copyright (c) 2024, Emmanuel Schweitzer and salesforce.com, inc.
+ * Copyright (c) 2024-2025, Emmanuel Schweitzer and salesforce.com, inc.
  * All rights reserved.
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-
-/* notes
-      field-level-help-position={_propSetMap.helpTextPos} not supported
-      required-label={allCustomLabelsUtil.OmniRequired} not suppported
-      icon-name="utility:info" not supported
-*/
 
 import SfGpsDsFormFormulaOsN from "c/sfGpsDsFormFormulaOsN";
 import tmpl from "./sfGpsDsAuQldFormFormulaOsN.html";
--- sfGpsDsAuQld/main/default/lwc/forms/lwc/sfGpsDsAuQldFormTextOsN/sfGpsDsAuQldFormTextOsN.js
+++ sfGpsDsAuQld/main/default/lwc/forms/lwc/sfGpsDsAuQldFormTextOsN/sfGpsDsAuQldFormTextOsN.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024, Emmanuel Schweitzer and salesforce.com, inc.
+ * Copyright (c) 2024-2025, Emmanuel Schweitzer and salesforce.com, inc.
  * All rights reserved.
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
@@ -13,5 +13,13 @@ export default class extends SfGpsDsFormTextOsN {
 
   render() {
     return tmpl;
+  }
+
+  connectedCallback() {
+    if (super.connectedCallback) {
+      super.connectedCallback();
+    }
+
+    this._readOnlyClass = "sfgpsdsauqld-read-only";
   }
 }
