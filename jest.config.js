41ae9fdc48de6ea6629b79b81d6304eb53a5f81b
Emmanuel Schweitzer <eschweitzer78@yahoo.com>
2025-04-22 16:31:39 +1000

    Latest sass, jest and ripple 2 for build

--- jest.config.js
+++ jest.config.js
@@ -2,8 +2,10 @@ const { jestConfig } = require("@salesforce/sfdx-lwc-jest/config");
 const ro = require("resize-observer-polyfill");
 
 const setupFilesAfterEnv = jestConfig.setupFilesAfterEnv || [];
-setupFilesAfterEnv.push("<rootDir>/jest-sa11y-setup.js");
 setupFilesAfterEnv.push("<rootDir>/jest-CSS-setup.js");
+setupFilesAfterEnv.push("<rootDir>/jest-crypto-setup.js");
+setupFilesAfterEnv.push("<rootDir>/jest-matchers-setup.js");
+setupFilesAfterEnv.push("<rootDir>/jest-sa11y-setup.js");
 
 module.exports = {
   ...jestConfig,
@@ -19,8 +21,12 @@ module.exports = {
     "^@salesforce/community/(.*)": "<rootDir>/__test__/jest-mocks/community/$1",
     "^omnistudio/omniscriptBaseMixin$":
       "<rootDir>/__test__/jest-mocks/omnistudio/omniscriptBaseMixin",
+    "^omnistudio/pubsub$": "<rootDir>/__test__/jest-mocks/omnistudio/pubsub",
     "^omnistudio/radioGroup$":
       "<rootDir>/__test__/jest-mocks/omnistudio/radioGroup",
+    "^omnistudio/salesforceUtils$":
+      "<rootDir>/__test__/jest-mocks/omnistudio/salesforceUtils",
+    "^omnistudio/utility$": "<rootDir>/__test__/jest-mocks/omnistudio/utility",
     "^lightning/combobox$":
       "<rootDir>/__test__/jest-mocks/lightning/combobox/combobox",
     "^lightning/primitiveIcon$":
