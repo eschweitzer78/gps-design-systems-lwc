74cf34689b1e498538714a8dbf7c4cae05462bb7
Emmanuel Schweitzer <eschweitzer78@yahoo.com>
2025-04-22 21:49:44 +1000

    Vic2 searchable  combobox

--- sfGpsDsAuVic2/main/default/lwc/forms/lwc/sfGpsDsAuVic2ComboboxOsN/sfGpsDsAuVic2ComboboxOsN.js
+++ sfGpsDsAuVic2/main/default/lwc/forms/lwc/sfGpsDsAuVic2ComboboxOsN/sfGpsDsAuVic2ComboboxOsN.js
@@ -5,9 +5,13 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
+import { api } from "lwc";
 import SfGpsDsAuVic2RplDropdownOsN from "c/sfGpsDsAuVic2RplDropdownOsN";
 import SfGpsDsOmniInputMixinOsN from "c/sfGpsDsOmniInputMixinOsN";
-import { api } from "lwc";
+import { normaliseBoolean } from "c/sfGpsDsHelpersOs";
+
+const SEARCHABLE_DEFAULT = false;
+const SEARCHABLE = Symbol("searchable");
 
 export default class extends SfGpsDsOmniInputMixinOsN(
   SfGpsDsAuVic2RplDropdownOsN
@@ -19,5 +23,39 @@ export default class extends SfGpsDsOmniInputMixinOsN(
 
   set options(value) {
     this._options = value;
+  }
+
+  _searchableOriginal;
+  _searchable = SEARCHABLE_DEFAULT;
+
+  @api
+  get searchable() {
+    return this._searchableOriginal;
+  }
+
+  set searchable(value) {
+    this._searchableOriginal = value;
+    const searchable = normaliseBoolean(value, {
+      acceptString: true,
+      fallbackValue: SEARCHABLE_DEFAULT
+    });
+
+    super.mode = searchable ? "filter-auto" : "lookup";
+    this[SEARCHABLE] = searchable;
+  }
+
+  @api
+  get mode() {
+    return super.mode;
+  }
+
+  set mode(value) {
+    super.mode = this[SEARCHABLE] ? "filter-auto" : "lookup";
+  }
+
+  /* lifecycle */
+
+  connectedCallback() {
+    super.mode = this[SEARCHABLE] ? "filter-auto" : "lookup";
   }
 }
