a6b4af7b037cbd84a1bf543e125b3bb3c066330e
Emmanuel Schweitzer <eschweitzer78@yahoo.com>
2025-05-02 11:06:12 +1000

    QLD nav issue on home and level 1 with mobile

--- sfGpsDsAuQld/main/default/lwc/sfGpsDsAuQldMainNav/lwc/sfGpsDsAuQldMainNav/sfGpsDsAuQldMainNav.js
+++ sfGpsDsAuQld/main/default/lwc/sfGpsDsAuQldMainNav/lwc/sfGpsDsAuQldMainNav/sfGpsDsAuQldMainNav.js
@@ -242,6 +242,7 @@ export default class extends LightningElement {
   @api close() {
     // eslint-disable-next-line @lwc/lwc/no-api-reassignments
     this.isActive = false;
+    this._open = false;
 
     // eslint-disable-next-line guard-for-in
     for (let prop in this._mapItems) {
@@ -259,6 +260,7 @@ export default class extends LightningElement {
   handleClickNavigate(event) {
     event.preventDefault();
 
+    console.log("> handleClickNavigate");
     this.close();
 
     const closeMenuEvent = new CustomEvent("closemenu");
@@ -266,6 +268,7 @@ export default class extends LightningElement {
 
     let index = event.currentTarget.dataset.ndx;
     this.dispatchEvent(new CustomEvent("navigate", { detail: index }));
+    console.log("< handleClickNavigate");
   }
 
   handleClick(event) {
@@ -305,7 +308,7 @@ export default class extends LightningElement {
   /* methods */
 
   handleCloseNav() {
-    this._open = false;
+    this.close();
   }
 
   handleMainNavToggle() {
