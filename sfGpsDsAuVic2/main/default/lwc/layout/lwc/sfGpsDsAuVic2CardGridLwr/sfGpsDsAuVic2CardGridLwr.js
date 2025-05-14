e12533da6786b6544308b4aac6cf0f609558ed21
Emmanuel Schweitzer <eschweitzer78@yahoo.com>
2025-05-14 20:59:10 +1000

    VIC2 trying to sort out card grid and card layout

--- sfGpsDsAuVic2/main/default/lwc/layout/lwc/sfGpsDsAuVic2CardGridLwr/sfGpsDsAuVic2CardGridLwr.js
+++ sfGpsDsAuVic2/main/default/lwc/layout/lwc/sfGpsDsAuVic2CardGridLwr/sfGpsDsAuVic2CardGridLwr.js
@@ -1,21 +1,51 @@
 import { api } from "lwc";
 import SfGpsDsLwc from "c/sfGpsDsLwc";
 
+const DEBUG = false;
+const CLASS_NAME = "sfGpsDsAuVic2CardGridLwr";
+
 /**
  * @slot Grid-Content
  */
 export default class extends SfGpsDsLwc {
-  @api hasSidebar;
-  @api className;
+  static renderMode = "light";
 
-  /* computed */
+  /* api: hasSideBar */
 
-  get computedClassName() {
-    return {
-      "rpl-layout-card-grid": true,
-      "rpl-layout-card-grid--has-sidebar": this.hasSidebar,
-      [this.className]: this.className
-    };
+  _hasSideBar;
+
+  @api
+  get hasSidebar() {
+    return this._hasSideBar;
+  }
+
+  set hasSidebar(value) {
+    if (this._hasSideBar && !value)
+      this.classList.remove("rpl-layout-card-grid--has-sidebar");
+    else if (!this._hasSideBar && value)
+      this.classList.add("rpl-layout-card-grid--has-sidebar");
+
+    this._hasSideBar = value;
+  }
+
+  /* api: className */
+
+  _className;
+
+  @api
+  get className() {
+    return this._className;
+  }
+
+  set className(value) {
+    if (DEBUG)
+      console.debug(CLASS_NAME, "> set className", value, this._className);
+
+    if (this._className) this.classList.remove(...this._className.split(" "));
+    this._className = value;
+    if (this._className) this.classList.add(...this._className.split(" "));
+
+    if (DEBUG) console.debug(CLASS_NAME, "< set className", this._className);
   }
 
   /* lifecycle */
@@ -24,6 +54,6 @@ export default class extends SfGpsDsLwc {
     this._isLwrOnly = true;
     super.connectedCallback();
 
-    this.classList.add("vic2-scope");
+    this.classList.add("rpl-layout-card-grid");
   }
 }
