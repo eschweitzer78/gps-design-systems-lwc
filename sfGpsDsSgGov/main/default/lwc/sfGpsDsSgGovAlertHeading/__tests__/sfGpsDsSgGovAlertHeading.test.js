4380b22693dc8fb6099b5d8136e5a9730bfbf6f0
Emmanuel Schweitzer <eschweitzer78@yahoo.com>
2025-06-19 17:55:39 +1000

    refactor tests sg

--- sfGpsDsSgGov/main/default/lwc/sfGpsDsSgGovAlertHeading/__tests__/sfGpsDsSgGovAlertHeading.test.js
+++ sfGpsDsSgGov/main/default/lwc/sfGpsDsSgGovAlertHeading/__tests__/sfGpsDsSgGovAlertHeading.test.js
@@ -1,4 +1,4 @@
-import { createElement } from "lwc";
+import { createElement } from "@lwc/engine-dom";
 import SfGpsDsSgGovAlertHeading from "c/sfGpsDsSgGovAlertHeading";
 
 describe("c-sf-gps-ds-sg-gov-alert-heading", () => {
