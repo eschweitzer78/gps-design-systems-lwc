d72057a2f5551b00dba610ac618ea91b0fa7812f
Emmanuel Schweitzer <eschweitzer78@yahoo.com>
2025-08-04 13:19:00 +1000

    Fix NSW DS date min max formatting, introduce NSW DS form step no buttons

---
+++ sfGpsDsAuNsw/main/default/lwc/forms/lwc/sfGpsDsAuNswFormStepNoButtonOsN/sfGpsDsAuNswFormStepNoButtonOsN.js
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2025, Emmanuel Schweitzer and salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+
+// Setup:
+// in omniscript designer, go to setup tab and in the Element Type To LWC Component Mapping section
+// set Step as the ElementType and this LWC as the Lightning Web Component
+
+import SfGpsDsAuNswFormStepOsN from "c/sfGpsDsAuNswFormStepOsN";
+
+export default class extends SfGpsDsAuNswFormStepOsN {
+  get showPrev() {
+    return false;
+  }
+
+  get showNext() {
+    return false;
+  }
+
+  get showSave() {
+    return false;
+  }
+
+  get computedShowSep() {
+    return false;
+  }
+}
