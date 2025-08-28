e440099fd8c4f9c88ace4451e3022509e256033b
Emmanuel Schweitzer <eschweitzer78@yahoo.com>
2025-08-28 14:24:56 +1000

    v64

--- sfGpsDs/main/default/lwc/typings/SfGpsDsIntegrationProcController.runIntegrationProcedure.d.tsx
+++
@@ -1,7 +0,0 @@
-declare module "@salesforce/apex/sfGpsDsIntegrationProcController.runIntegrationProcedure" {
-  export default function runIntegrationProcedure(options: {
-    ipName: string,
-    input: object,
-    options: object
-  }): Promise<any>;
-}
