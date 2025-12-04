166bcc639f37d48e01662c089fa14e518fc57f75
Emmanuel Schweitzer <eschweitzer78@yahoo.com>
2025-12-04 12:05:37 +1000

    Headless components and backend logic for auth

---
+++ .sfdx/typings/lwc/apex/SfGpsDsCommunitiesController.d.ts
@@ -0,0 +1,9 @@
+declare module "@salesforce/apex/SfGpsDsCommunitiesController.getAuthConfig" {
+  export default function getAuthConfig(): Promise<any>;
+}
+declare module "@salesforce/apex/SfGpsDsCommunitiesController.userLogin" {
+  export default function userLogin(param: {username: any, password: any}): Promise<any>;
+}
+declare module "@salesforce/apex/SfGpsDsCommunitiesController.userForgotPassword" {
+  export default function userForgotPassword(param: {username: any}): Promise<any>;
+}
