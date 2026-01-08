54133dcf48936df9e2a3dc7e9a10f7d4834ed6eb
Emmanuel Schweitzer <eschweitzer78@yahoo.com>
2026-01-08 12:12:38 +1000

    NSW DS tab content lazy loading can create issue when markup is passed through daisy-chained slots

--- sfGpsDsAuNsw/main/default/lwc/typings/sfGpsDsAuNswTabLwr.d.ts
+++ sfGpsDsAuNsw/main/default/lwc/typings/sfGpsDsAuNswTabLwr.d.ts
@@ -1,12 +1,18 @@
 declare module "c/sfGpsDsAuNswTabLwr" { 
   import type SfGpsDsTabLwr from "c/sfGpsDsTabLwr"; 
+  import type { PropertyAccessor } from "c/sfGpsDsElement";
 
   export default 
   class SfGpsDsAuNswTabLwr 
   extends SfGpsDsTabLwr {
+    disableLazyLoading?: boolean;
+
     // private
+
+    _disableLazyLoading: PropertyAccessor<boolean>;
 
     static renderMode: "light" | "shadow"
     get computedClassName(): any;
+    get computedLoadContent(): any;
   }
 }
