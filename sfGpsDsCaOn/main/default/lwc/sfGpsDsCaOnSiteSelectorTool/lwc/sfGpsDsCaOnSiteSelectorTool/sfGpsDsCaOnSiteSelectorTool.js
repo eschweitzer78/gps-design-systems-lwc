32a611746732723ac9282748fc9596446cbe95bd
sschupbach <sschupbach@gmail.com>
2026-01-30 10:49:20 -0600

    Remove Jeremy Blankenship from copyright and author comments
    He has not worked on this project.

--- sfGpsDsCaOn/main/default/lwc/sfGpsDsCaOnSiteSelectorTool/lwc/sfGpsDsCaOnSiteSelectorTool/sfGpsDsCaOnSiteSelectorTool.js
+++ sfGpsDsCaOn/main/default/lwc/sfGpsDsCaOnSiteSelectorTool/lwc/sfGpsDsCaOnSiteSelectorTool/sfGpsDsCaOnSiteSelectorTool.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2026, Shannon Schupbach, Jeremy Blankenship and salesforce.com, inc.
+ * Copyright (c) 2026, Shannon Schupbach and salesforce.com, inc.
  * All rights reserved.
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
