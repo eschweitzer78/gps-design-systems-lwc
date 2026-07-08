ce94068cd71c37adfe8c5891d818549e51104830
Emmanuel Schweitzer <eschweitzer78@yahoo.com>
2026-07-08 22:01:35 +1000

    Claude review of NSW Pagination

--- sfGpsDsAuNsw/main/default/lwc/sfGpsDsAuNswPagination/lwc/sfGpsDsAuNswPaginationComm/sfGpsDsAuNswPaginationComm.ts
+++ sfGpsDsAuNsw/main/default/lwc/sfGpsDsAuNswPagination/lwc/sfGpsDsAuNswPaginationComm/sfGpsDsAuNswPaginationComm.ts
@@ -1,5 +1,12 @@
-import { 
-  api 
+/*
+ * Copyright (c) 2022-2025, Emmanuel Schweitzer and salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+
+import {
+  api
 } from "lwc";
 import SfGpsDsLwc from "c/sfGpsDsLwc";
 
