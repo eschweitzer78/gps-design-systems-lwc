5a70c978402d55626fb25cddfde653b7cc54cca6
Emmanuel Schweitzer <eschweitzer78@yahoo.com>
2026-07-08 21:40:09 +1000

    Claude review of NSW Header Main Nav

--- sfGpsDsAuNsw/main/default/lwc/sfGpsDsAuNswHeaderMainNav/lwc/sfGpsDsAuNswHeaderProfile/sfGpsDsAuNswHeaderProfile.ts
+++ sfGpsDsAuNsw/main/default/lwc/sfGpsDsAuNswHeaderMainNav/lwc/sfGpsDsAuNswHeaderProfile/sfGpsDsAuNswHeaderProfile.ts
@@ -1,6 +1,12 @@
-import { 
-  api, 
-  track 
+/*
+ * Copyright (c) 2022-2025, Emmanuel Schweitzer and salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+
+import {
+  api
 } from "lwc";
 import SfGpsDsElement from "c/sfGpsDsElement";
 import OnClickOutside from "c/sfGpsDsOnClickOutside";
@@ -32,8 +38,6 @@ extends SfGpsDsElement {
   @api 
   className?: string;
 
-  // @ts-ignore
-  @track 
   _isOpen = false;
 
   /* event management */
